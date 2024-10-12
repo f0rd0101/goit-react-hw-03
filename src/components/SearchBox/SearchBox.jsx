edf23bb01b4589d50db9393b94fb0fcc7fedd862
Nikita Ivashchenko <melancholic8078@gmail.com>
2024-10-13 01:12:35 +0300

    added serchbox markup

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import s from '../SearchBox/SearchBox.module.css'
 
 const SearchBox = () => {
   return (
     <div>
-      
+      <h4 className={s.mainSearchBoxTile}>Find contacts by name</h4>
+      <form>
+        <label>
+          <input type="text" />
+        </label>
+      </form>
     </div>
   )
 }
